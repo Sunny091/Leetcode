2f0a06c55785aec21a3a0b88446904fac8a00d53
Sunny Shen <s890919@gmail.com>
2025-03-27 15:57:52 +0800

    push

---
+++ 10. Regular Expression Matching/RegexMatch.java
@@ -0,0 +1,34 @@
+public class RegexMatch {
+    public static boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;
+
+        for (int j = 2; j <= n; j += 2) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aab", "c*a*b")); // true
+        System.out.println(isMatch("mississippi", "mis*is*p*.")); // false
+    }
+}
---
+++ 7. Reverse Integer/ReverseInteger.java
@@ -0,0 +1,29 @@
+public class ReverseInteger {
+    public static int reverse(int x) {
+        int result = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // 檢查是否會溢位
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            result = result * 10 + digit;
+        }
+
+        return result;
+    }
+
+    // 測試用 main
+    public static void main(String[] args) {
+        int input = 123;
+        int output = reverse(input);
+        System.out.println("Reversed: " + output);
+    }
+}
