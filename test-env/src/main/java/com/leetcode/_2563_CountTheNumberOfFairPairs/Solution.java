7013ee15f043a0f6b0b7a58307a14f32dab4d050
Sunny Shen <s890919@gmail.com>
2025-04-22 09:09:32 +0800

    2563 2_version, correct and Runtime beats 58.49%, Memory beats 64.6%

--- test-env/src/main/java/com/leetcode/_2563_CountTheNumberOfFairPairs/Solution.java
+++ test-env/src/main/java/com/leetcode/_2563_CountTheNumberOfFairPairs/Solution.java
@@ -6,21 +6,37 @@ public class Solution {
     public static long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
         long result = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            int start = i + 1;
-            while (start < nums.length && nums[i] + nums[start] < lower) {
-                start++;
-            }
-
-            int end = nums.length - 1;
-            while (end >= start && nums[i] + nums[end] > upper) {
-                end--;
-            }
-
-            if (start <= end) {
-                result += (end - start + 1);
+        for (int i = 0; i < nums.length; i++) {
+            long left = leftIndex(nums, i + 1, nums.length - 1, lower - nums[i]);
+            long right = rightIndex(nums, i + 1, nums.length - 1, upper - nums[i]);
+            if (left <= right) {
+                result += (right - left + 1);
             }
         }
         return result;
+    }
+
+    public static long leftIndex(int[] nums, int low, int high, int target) {
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    private static long rightIndex(int[] nums, int low, int high, int target) {
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            if (nums[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
     }
 }
