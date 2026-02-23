b1e3fecc196342a3de7ba51ed8075d2eeaac5381
Sunny Shen <s890919@gmail.com>
2026-02-23 13:20:46 +0800

    update: 283 version 2, success and complete

--- python/problems/_283_Move_Zeroes/solution.py
+++ python/problems/_283_Move_Zeroes/solution.py
@@ -8,7 +8,10 @@ class Solution:
 
         TODO: Implement your solution here
         """
-        for i in nums:
-            if i == 0:
-                nums.remove(i)
-                nums.append(0)
+        left = 0
+        
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+            
+                left += 1
