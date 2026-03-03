85ebf04e6505eb644aa0a43d7f8c5e3ac125938e
Sunny Shen <s890919@gmail.com>
2026-03-03 19:40:27 +0800

    add: 1545 version 2, success and complete

--- python/problems/_1545_Find_Kth_Bit_in_Nth_Binary_String/solution.py
+++ python/problems/_1545_Find_Kth_Bit_in_Nth_Binary_String/solution.py
@@ -1,22 +1,15 @@
-from typing import List
-import math
-
-
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        """
-        LeetCode 1545. Find Kth Bit in Nth Binary String
-
-        TODO: Implement your solution here
-        """
-        bound = math.ceil(k ** 0.5)
-        s = "0"
-        if k == 1:
-            return s
-        if type(k ** 0.5) == int:
+        if n == 1:
+            return "0"
+        
+        mid = 1 << (n - 1)
+        if k == mid:
             return "1"
-        for i in range(1, bound + 1):
-            s = s + "1" + s[::-1].translate(str.maketrans("01", "10"))
-            print(s)
-            if len(s) >= k:
-                return s[k - 1]
+        
+        if k < mid:
+            return self.findKthBit(n - 1, k)
+        
+        mirrored = (1 << n) - k
+        bit = self.findKthBit(n - 1, mirrored)
+        return "1" if bit == "0" else "0"
