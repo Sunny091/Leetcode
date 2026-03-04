14f71daa22959c5ba8916b91f2a82ed68e30838d
Sunny Shen <s890919@gmail.com>
2026-03-04 17:46:14 +0800

    add: 1582 version 1, success and complete

---
+++ python/problems/_1582_Special_Positions_in_a_Binary_Matrix/test_solution.py
@@ -0,0 +1,27 @@
+import pytest
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent))
+
+from solution import Solution
+
+
+class TestSolution:
+    def setup_method(self):
+        self.sol = Solution()
+
+    def test_example_1(self):
+        # TODO: Add test case
+        mat = [[1,0,0],[0,0,1],[1,0,0]]
+        expected = 1
+        assert self.sol.numSpecial(mat) == expected
+
+    def test_example_2(self):
+        # TODO: Add test case
+        mat = [[1,0,0],[0,1,0],[0,0,1]]
+        expected = 3
+        assert self.sol.numSpecial(mat) == expected
+
+    # def test_example_3(self):
+    #     # TODO: Add test case
+    #     pass
